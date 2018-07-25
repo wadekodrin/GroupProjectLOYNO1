07242faaf95451475acd25591926fc8d797e6a31
anna o mcc <annaosbornemccarthy@gmail.com>
2018-07-25 11:27:46 -0500

    ready to deploy

--- GP1/Public/script.js
+++ GP1/Public/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-console.log("hello1");
-    onClick()
+console.log("load function");
+    onClick();
+    onLoad();
+    // scrollFunction();
+ 
 
 });
-
-function onClick(){        
-    $("#All_Companies").click(function(){
+function onLoad(){
     $('.company_container').empty();
-        // console.log($('#uemail').val());
-        // var buttoninputvar = $('#buttoninputid').val();
-        console.log("hello");
+        console.log("On Load: empty company containers");
         $.ajax({
             method: "GET",
             url: "/Company_Name_All", //url is /routename
@@ -19,27 +18,24 @@ function onClick(){
                 'Access-Control-Allow-Headers':'*'
             },
             success: function(data) { 
-                console.log(data);
                 for(var i = 0; i < data.length; i++){
-                    
                     var myHtml = 
-                        "<h4>"+data[i].Company_Name+ "</h4>"+ 
-                        "<h1>"+data[i].Website+"</h1>"+
-                        "<img class = 'logo' scr ='"+data[i].LOG_URL+"'>"
+                        "<div class='card' div class id='companycard'>"+
+                        "<img src ='"+data[i].LOG_URL+"' id = 'logo_url'>"+
+                        "<h4 id='company_name'>"+data[i].Company_Name+"</h4>"+ 
+                        "<h1 id= 'link_name'> <a href= '"+data[i].Website+"'>"+data[i].Website+" </a> </h1>"+
+                        "</div>"                    
                     $('.company_container').append(myHtml)
                 }
             },
             error: function(xhr, status, error) { 
                 console.log("ERROR: ", error) 
             }
+
         })
-    })
-console.log("hello2");
-    $("#All_Positions").click(function(){
-        $('.company_container').empty();
-        // console.log($('#uemail').val());
-        // var buttoninputvar = $('#buttoninputid').val();
-        console.log("hello_listing");
+
+    $('.position_container').empty();
+         console.log("On Load: empty position containers");
         $.ajax({
             method: "GET",
             url: "/Position_All", //url is /routename
@@ -49,21 +45,213 @@ console.log("hello2");
                 'Access-Control-Allow-Headers':'*'
             },
             success: function(data) { 
-                console.log(data);
+               for(var i = 0; i < data.length; i++){
+                    var myHtml =
+                        "<div class='card' div class id='position_card'>"+
+                        "<img src ='"+data[i].LOG_URL+"' id = 'logo_url'>"+ 
+                        "<h4 id = 'company_name'>"+data[i].Company_Name+ "</h4>"+ 
+                        "<h4 id ='position'>"+data[i].Position+"</h4>"+
+                        "<h4 id='position_source'> <a href='"+data[i].Position_Source+"'> link to apply: <br> <br>"+data[i].Position_Source+"</a href> </h4>"+
+                        "</div>"
+                    $('.position_container').append(myHtml)
+                }
+            },
+            error: function(xhr, status, error) { 
+                console.log("ERROR: ", error) 
+            }
+
+        })
+    // $('#input_co').click(function(){
+        // $('.NTID_D_con').empty();
+        //     console.log("empty NTID_D_con on click");
+
+        $.ajax({
+            method: "GET",
+            url: "/Company_Name_All",
+            dataType: 'json',
+            headers: {
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Headers':'*'
+            },
+            success: function(data) { 
+
+                for(var i = 0; i < data.length; i++){
+                    var myHtml =
+                        "<option value='"+data[i].Company_ID+"''>"+data[i].Company_Name+"</option>"
+ 
+                        $('#input_co').append(myHtml);
+
+
+
+                        // console.log(myHtml);
+                        // var wadeElm = $('.NTD');
+                        // console.log(wadeElm[i].value);
+                        // //console.log(.val());
+                        
+                  
+                }
+                
+            },
+            error: function(xhr, status, error) { 
+                console.log("ERROR: ", error) 
+            }
+        })
+        $.ajax({
+            method: "GET",
+            url: "/NTID_Name_ALL",
+            dataType: 'json',
+            headers: {
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Headers':'*'
+            },
+            success: function(data) { 
+
+                for(var i = 0; i < data.length; i++){
+                    var myHtml =
+                        "<option value='"+data[i].NTID+"''>"+data[i].Title+"</option>"
+ 
+                        $('#NTID_D_i').append(myHtml);
+
+
+
+                        // console.log(myHtml);
+                        // var wadeElm = $('.NTD');
+                        // console.log(wadeElm[i].value);
+                        // //console.log(.val());
+                        
+                  
+                }
+                
+            },
+            error: function(xhr, status, error) { 
+                console.log("ERROR: ", error) 
+            }
+        })
+
+};
+function onClick(){
+
+    $('#cosearchbtn').click(function(){
+        $('.company_container').empty();
+            console.log("empty company container on click");
+        console.log($('#searchinput').val());
+        var searchinput = $('#searchinput').val();
+        console.log(searchinput);
+        $.ajax({
+            method: "GET",
+            url: "/Company_Name_Search?Company_Name="+searchinput, //url is /routename
+            dataType: 'json',
+            headers: {
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Headers':'*'
+            },
+            success: function(data) { 
+                console.log(data);                
+                var resultnum = data.length;
+                console.log(resultnum);
+                $('#results').html(resultnum+" result(s)")
                 for(var i = 0; i < data.length; i++){
                     
-                    var myHtmllisting = 
-                        "<h1>"+data[i].Position+"</h1>"+
-                        "<h1>"+data[i].Source+"</h1>"+
-                        "<h1>"+data[i].Company_Name+"</h1>"+
-                        "<h1>"+data[i].Website+"</h1>"+
-                        "<img class = 'logo' scr ='"+data[i].LOG_URL+"'>"
-                    $('.position_container').append(myHtmllisting)
-                 }
+                    var myHtml = 
+                        "<div class='card' div class id='companycard'>"+
+                        "<img src ='"+data[i].LOG_URL+"' id = 'logo_url'>"+
+                        "<h4 id='company_name'>"+data[i].Company_Name+"</h4>"+ 
+                        "<h1 id= 'link_name'> <a href= '"+data[i].Website+"'>"+data[i].Website+" </a> </h1>"+
+                        "</div>"
+                    $('.company_container').append(myHtml)
+                }
             },
             error: function(xhr, status, error) { 
                 console.log("ERROR: ", error) 
             }
         })
     })
+    $('#posearchbtn').click(function(){
+        $('.position_container').empty();
+            console.log("empty position_container on click");
+        console.log("typed data is "+$('#searchinput').val());
+        var searchinput = $('#searchinput').val();
+        console.log("input data into function is "+searchinput);
+        $.ajax({
+            method: "GET",
+            url: "/Position_Search?Position="+searchinput, //url is /routename
+            dataType: 'json',
+            headers: {
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Headers':'*'
+            },
+            success: function(data) { 
+                console.log(data);
+                var resultnum = data.length;
+                console.log(resultnum);
+                 $('#results').html(resultnum+" result(s)")
+                for(var i = 0; i < data.length; i++){
+
+                    var myHtml =
+                        "<div class='card' div class id='position_card'>"+
+                        "<img src ='"+data[i].LOG_URL+"' id = 'logo_url'>"+ 
+                        "<h4 id = 'company_name'>"+data[i].Company_Name+ "</h4>"+ 
+                        "<h4 id ='position'>"+data[i].Position+"</h4>"+
+                        "<h4 id='position_source'> <a href='"+data[i].Position_Source+"'> link to apply: <br> <br>"+data[i].Position_Source+"</a href> </h4>"+
+                        "</div>"
+                    $('.position_container').append(myHtml)
+                }
+            },
+            error: function(xhr, status, error) { 
+                console.log("ERROR: ", error) 
+            }
+        })
+    })
+
+
+    $('#submitform').click(function(){
+        console.log($('#input_co').val());
+        console.log($('#input_pos').val());
+        console.log($('#NTID_D_i').val());
+        console.log($('#input_website').val());
+        
+
+        var passJSON = {         
+            "CID_D_i":Number($('#input_co').val()),
+            "pos_input_i":$('#input_pos').val(),
+            "pos_web_input_i":$('#input_website').val(),
+            "NTID_D_i":Number($('#NTID_D_i').val())
+
+        };
+        console.log(passJSON," from varpassJSON");
+        $.post("/New_Pos", passJSON ,function(data, status){
+            console.log(data);
+        });
+        // $.ajax({
+        //     method: "POST",
+        //     url: "/New_Pos", //url is /routename
+        //     dataType: 'json',
+        //     headers: {
+        //         'Content-Type':'application/json',
+        //         'Access-Control-Allow-Headers':'*'
+        //     },
+        //     data: wadesJSON,
+        //     success: function(data) { 
+        //         console.log("success script.js")
+        //     },
+        //     error: function(xhr, status, error) { 
+        //         console.log("ERROR: ", error) 
+        //     }
+        // })
+    })
 }
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("myBtn").style.display = "block";
+//     } else {
+//         document.getElementById("myBtn").style.display = "none";
+//     }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// } 
--- GP1/app.js
+++ GP1/app.js
@@ -3,11 +3,16 @@ var mysql = require('mysql');
 const app = express ();
 
 var pool = mysql.createPool({
-	host	: 'localhost',
-	user	: 'root',
-	password: 'root',
-	database: 'gp1' //database name in MAMP
+	host	: process.env.db_host,
+	user	: process.env.db_username,
+	password: process.env.db_password,
+	database: process.env.db_name
 });
+
+var bodyParser = require('body-parser');//allows use of body parser, similar to express
+
+app.use(bodyParser.urlencoded({limit: '50mb',extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
 
 var path = require('path');
 
@@ -28,6 +33,16 @@ app.get('/index2', function(req, res) {
  
  })
 
+app.get('/index3', function(req, res) {
+
+   res.sendFile(path.resolve('Public/index3.html'));
+ 
+ })
+app.get('/index4', function(req, res) {
+
+   res.sendFile(path.resolve('Public/index4.html'));
+ 
+ })
 app.get('/buttontest', function(req, res) {
 
    res.sendFile(path.resolve('Public/buttontest.html'));
@@ -47,7 +62,17 @@ app.get ('/Company_Name_All', function (req,res){ //'/routetest' name of route
 		});
 	});
 });
+app.get ('/NTID_Name_ALL', function (req,res){ //'/routetest' name of route
 
+	pool.getConnection(function(err, connection){
+		connection.query('SELECT * FROM NOLATECH_CALC ORDER BY NOLATECH_CALC.Title ASC', function (error, results, fields){
+			connection.release();
+			if(!err) {
+				res.json(results);
+			}
+		});
+	});
+});
 app.get ('/Position_All', function (req,res){ //'/routetest' name of route
 //localhost:3000/Position_All?Company_Name=wade after route name, ? then key name (case sensitive) = value. Value is then passed to route function via req.query."key"
 //localhost:3000/Position_All?Company_Name=wade&key=value&key=value
@@ -62,9 +87,9 @@ app.get ('/Position_All', function (req,res){ //'/routetest' name of route
 });
 
 app.get ('/Company_Name_Search', function (req,res){ 
-	var searchterm = req.query.Company_Name;
+	var searchinput = req.query.Company_Name;
 	pool.getConnection(function(err, connection){
-		var sqlquery= "SELECT * FROM companies WHERE Companies.Company_Name='"+searchterm+"' ORDER BY companies.Company_Name ASC";
+		var sqlquery= "SELECT * FROM companies WHERE Companies.Company_Name LIKE '%"+searchinput+"%' ORDER BY companies.Company_Name ASC";
 		connection.query(sqlquery, function (error, results, fields){
 			connection.release();
 			if(!err) {
@@ -74,9 +99,9 @@ app.get ('/Company_Name_Search', function (req,res){
 	});
 });
 app.get ('/Company_Name_Openings_Search', function (req,res){ 
-	var searchterm = req.query.Company_Name;
+	var searchinput = req.query.Company_Name;
 	pool.getConnection(function(err, connection){
-		var sqlquery= "SELECT * FROM openings INNER JOIN companies on openings.Company_ID = companies.Company_ID WHERE companies.Company_Name ='"+searchterm+"' ORDER BY companies.Company_Name ASC";
+		var sqlquery= "SELECT * FROM openings INNER JOIN companies on openings.Company_ID = companies.Company_ID WHERE companies.Company_Name LIKE '%"+searchinput+"%' ORDER BY companies.Company_Name ASC";
 		console.log(sqlquery);
 		connection.query(sqlquery, function (error, results, fields){
 			connection.release();
@@ -87,9 +112,9 @@ app.get ('/Company_Name_Openings_Search', function (req,res){
 	});
 });
 app.get ('/Position_Search', function (req,res){ 
-	var searchterm = req.query.Position;
+	var searchinput = req.query.Position;
 	pool.getConnection(function(err, connection){ 
-		var sqlquery= "SELECT * FROM openings WHERE openings.Position='"+searchterm+"' ORDER BY openings.position ASC";
+		var sqlquery= "SELECT * FROM openings INNER JOIN companies on openings.Company_ID = companies.Company_ID WHERE openings.Position LIKE '%"+searchinput+"%' ORDER BY openings.position ASC";
 		console.log(sqlquery);
 		connection.query(sqlquery, function (error, results, fields){
 			connection.release();
@@ -100,9 +125,9 @@ app.get ('/Position_Search', function (req,res){
 	});
 });
 app.get ('/NTID_TITLE_SEARCH', function (req,res){ 
-	var searchterm = req.query.Title;
+	var searchinput = req.query.Title;
 	pool.getConnection(function(err, connection){
-		var sqlquery= "SELECT * FROM NOLATECH_CALC WHERE NOLATECH_CALC.Title='"+searchterm+"' ORDER BY nolatech_calc.Title ASC";
+		var sqlquery= "SELECT * FROM NOLATECH_CALC WHERE NOLATECH_CALC.Title LIKE '%"+searchinput+"%' ORDER BY nolatech_calc.Title ASC";
 		console.log(sqlquery);
 		connection.query(sqlquery, function (error, results, fields){
 			connection.release();
@@ -113,9 +138,9 @@ app.get ('/NTID_TITLE_SEARCH', function (req,res){
 	});
 });
 app.get ('/Openings_NT_TITLE', function (req,res){ 
-	var searchterm = req.query.Title;
+	var searchinput = req.query.Title;
 	pool.getConnection(function(err, connection){
-		var sqlquery= "SELECT * FROM openings INNER JOIN NOLATECH_CALC on openings.NTID = NOLATECH_CALC.NTID WHERE NOLATECH_CALC.title='"+searchterm+"' ORDER BY openings.NTID ASC";
+		var sqlquery= "SELECT * FROM openings INNER JOIN NOLATECH_CALC on openings.NTID = NOLATECH_CALC.NTID WHERE NOLATECH_CALC.title LIKE '%"+searchinput+"%' ORDER BY openings.NTID ASC";
 		console.log(sqlquery);
 		connection.query(sqlquery, function (error, results, fields){
 			connection.release();
@@ -125,7 +150,25 @@ app.get ('/Openings_NT_TITLE', function (req,res){
 		});
 	});
 });
-
+app.post ('/New_Pos', function (req,res){ 
+	var CID_D = req.body.CID_D_i; //CID_D_i var name listed in front, var CID_D defined in backend in this route
+	var pos_input = req.body.pos_input_i;
+	var pos_web_input = req.body.pos_web_input_i;
+	var NTID_D = req.body.NTID_D_i;
+	pool.getConnection(function(err, connection){
+		var sqlquery= "INSERT INTO Openings (Company_ID,Position,Position_Source,NTID) Values ("+CID_D+",'"+pos_input+"','"+pos_web_input+"',"+NTID_D+")"
+		console.log(sqlquery);
+		connection.query(sqlquery, function (error, results, fields){
+			if (error) {
+				console.log("ERROR", error);
+			}
+			connection.release();
+			if(!err) {
+				res.send({"success":"true"});
+			}
+		});
+	});
+});
 	/*console.log(req);
 	console.log('route test');
 	res.send('route test - this is the response');*/
@@ -136,4 +179,6 @@ app.get ('/Openings_NT_TITLE', function (req,res){
 // 	//res.send('route test2 2- this is the response');
 // });
 
-app.listen(3000, function(){ console.log('Example app listening on port 3000!')})
+var port = process.env.PORT || 3001
+
+app.listen(port, function(){ console.log('Example app listening on port 3001!')})
